5ba4d83c18a534613a6609447fc8b6452899f0ce
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-04-02 20:37:08 +0300

    added signature of classes implemented RequestStrategy

---
+++ Server/src/main/java/com/gv/archive/communication/strategies/implementations/DeleteRequestStrategy.java
@@ -0,0 +1,13 @@
+package com.gv.archive.communication.strategies.implementations;
+
+import com.gv.archive.communication.interfaces.Request;
+import com.gv.archive.communication.interfaces.Response;
+import com.gv.archive.communication.strategies.interfaces.RequestStrategy;
+
+public class DeleteRequestStrategy implements RequestStrategy{
+
+    @Override
+    public Response executeRequest(Request request) {
+        return null;
+    }
+}
