0aa5de502b0a359b7d4f185851e3a89fa053f12d
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-04-17 23:25:12 +0300

    added request handling strategies on server

--- Server/src/main/java/com/gv/archive/communication/strategies/implementations/GetRequestStrategy.java
+++ Server/src/main/java/com/gv/archive/communication/strategies/implementations/GetRequestStrategy.java
@@ -1,13 +1,29 @@
 package com.gv.archive.communication.strategies.implementations;
 
+import com.gv.archive.communication.implementations.BasicResponse;
 import com.gv.archive.communication.interfaces.Request;
 import com.gv.archive.communication.interfaces.Response;
 import com.gv.archive.communication.strategies.interfaces.RequestStrategy;
+import com.gv.archive.xml.parsers.factories.implementations.XMLDossierParserFactoryImpl;
+import com.gv.archive.xml.parsers.factories.interfaces.XMLDossierParserFactory;
+import com.gv.archive.xml.parsers.interfaces.XMLDossierParser;
 
 public class GetRequestStrategy implements RequestStrategy{
 
+    private final static String ALL_DOSSIERS_REQUEST_BODY = "dossiers";
+
     @Override
     public Response executeRequest(Request request) {
-        return null;
+        XMLDossierParserFactory factory = new XMLDossierParserFactoryImpl();
+        XMLDossierParser parser = factory.defineParserType(request.getXMLParser());
+
+        String login = request.getRequestBody();
+        Response response;
+        if(login.equals(ALL_DOSSIERS_REQUEST_BODY)){
+            response = new BasicResponse(parser.getDossiers());
+        } else {
+            response = new BasicResponse(parser.getDossier(login));
+        }
+        return response;
     }
 }
