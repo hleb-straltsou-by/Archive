c866a7d6b6433b2c06e569cc1f1d15fa7c949ab7
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-04-17 23:27:37 +0300

    added factories for request strategies on server

---
+++ Server/src/main/java/com/gv/archive/communication/factories/implementations/RequestStrategyFactoryImpl.java
@@ -0,0 +1,26 @@
+package com.gv.archive.communication.factories.implementations;
+
+import com.gv.archive.communication.factories.enums.RequestStrategyEnum;
+import com.gv.archive.communication.factories.interfaces.RequestStrategyFactory;
+import com.gv.archive.communication.interfaces.Request;
+import com.gv.archive.communication.strategies.interfaces.RequestStrategy;
+import com.gv.archive.logging.AppLogger;
+
+public class RequestStrategyFactoryImpl implements RequestStrategyFactory {
+
+    private RequestStrategyEnum strategyEnum;
+
+    public RequestStrategyFactoryImpl(){}
+
+    @Override
+    public RequestStrategy defineRequestStrategy(Request request) {
+        try{
+            strategyEnum = RequestStrategyEnum.valueOf(request.getRequestType());
+        } catch (IllegalArgumentException e){
+            strategyEnum = RequestStrategyEnum.EMPTY;
+            AppLogger.getLogger().error(e);
+        } finally {
+            return strategyEnum.getCurrentRequestStrategy();
+        }
+    }
+}
