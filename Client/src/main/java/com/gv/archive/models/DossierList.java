5665748caf5609454bd6ff9c64bedbd8aa24e7a6
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-04-17 23:33:40 +0300

    added models on client

---
+++ Client/src/main/java/com/gv/archive/models/DossierList.java
@@ -0,0 +1,28 @@
+package com.gv.archive.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DossierList {
+
+    private List<Dossier> list;
+
+    public DossierList(){
+        list = new ArrayList<Dossier>();
+    }
+
+    public Dossier get(int index){
+        if(index < 0 || list.size() <= index){
+            return null;
+        }
+        return list.get(index);
+    }
+
+    public void add(Dossier dossier){
+        list.add(dossier);
+    }
+
+    public List<Dossier> getList() {
+        return list;
+    }
+}
