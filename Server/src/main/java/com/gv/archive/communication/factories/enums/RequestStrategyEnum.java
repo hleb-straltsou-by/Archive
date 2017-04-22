a69a3a08cb706b1077f4801167a0e878dedf39c9
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-04-22 23:30:40 +0300

    update class RequestStrategyEnum for ioc features

--- Server/src/main/java/com/gv/archive/communication/factories/enums/RequestStrategyEnum.java
+++ Server/src/main/java/com/gv/archive/communication/factories/enums/RequestStrategyEnum.java
@@ -1,42 +1,57 @@
 package com.gv.archive.communication.factories.enums;
 
-import com.gv.archive.communication.strategies.implementations.*;
 import com.gv.archive.communication.strategies.interfaces.RequestStrategy;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public enum RequestStrategyEnum {
 
     GET{
         {
-            strategy = new GetRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(GET_REQUEST_STRATEGY_BEAN);
         }
     },
     PUT{
         {
-            strategy = new PutRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(PUT_REQUEST_STRATEGY_BEAN);
         }
     },
     POST{
         {
-            strategy = new PostRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(POST_REQUEST_STRATEGY_BEAN);
         }
     },
     DELETE{
         {
-            strategy = new DeleteRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(DELETE_REQUEST_STRATEGY_BEAN);
         }
     },
     LOGIN{
         {
-            strategy = new LoginRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(LOGIN_REQUEST_STRATEGY_BEAN);
         }
     },
     EMPTY{
         {
-            strategy = new EmptyRequestStrategy();
+            strategy = (RequestStrategy)context.getBean(EMPTY_REQUEST_STRATEGY_BEAN);
         }
-    },;
+    };
 
     RequestStrategy strategy;
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("IoC/request-context.xml");
+
+    private final static String DELETE_REQUEST_STRATEGY_BEAN = "deleteRequestStrategy";
+
+    private final static String EMPTY_REQUEST_STRATEGY_BEAN = "emptyRequestStrategy";
+
+    private final static String GET_REQUEST_STRATEGY_BEAN = "getRequestStrategy";
+
+    private final static String LOGIN_REQUEST_STRATEGY_BEAN = "loginRequestStrategy";
+
+    private final static String POST_REQUEST_STRATEGY_BEAN = "postRequestStrategy";
+
+    private final static String PUT_REQUEST_STRATEGY_BEAN = "putRequestStrategy";
 
     public RequestStrategy getCurrentRequestStrategy(){
         return strategy;
